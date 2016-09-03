55b23d5af0bafde41fabc8c5ad89ad83646ff4e7
kenthellck <dhanujayapraveen@gmail.com>
2016-09-03 21:53:35 +0530

    --DHANUJAYA-- Mul function is implemented please add subtraction function and commit ----

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 echo "<h1>Heading</h1><br>";
 
 echo "SUM IS : ".SUM(10,20);
+echo "Multiplication IS : ".MUL(50,33);
 
 function SUM($x,$y) {
     $sum=$x+$y;
     return $sum;
+}
+function MUL($x,$y) {
+    $mul=$x*$y;
+    return $mul;
 }
