f8e6f0ac2ca7bb0a17ccf8329d9e67bf50726334
shavindraSN <shavidnramms1@gmail.com>
2016-09-03 22:37:40 +0530

    Reomve unnecessory variables

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ echo "SUM IS : ".SUM(10,20);
 echo "Multiplication IS : ".MUL(50,33);
 
 function SUM($x,$y) {
-    $sum=$x+$y;
-    return $sum;
+    // sum variable removed
+    return ($x+$y);
 }
 function MUL($x,$y) {
     $mul=$x*$y;
